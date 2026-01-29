4e1f560783062d978b556815f9b918a688f632df
Bijo <3110.bijo@gmail.com>
2026-01-29 13:12:26 +0530

    Displayed String

--- base/base/urls.py
+++ base/base/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from polls.views import home
 
 urlpatterns = [
+    path('test/', home, name="home"),
     path('admin/', admin.site.urls),
 ]
